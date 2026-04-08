011011146913d2f294c120275bd033db04a337d7
Zainb Abdelwahab <zainbmaged114@gmail.com>
2026-04-08 18:18:11 +0200

    Added Tea with sugar to tasks

--- init.sql
+++ init.sql
@@ -1,8 +1,9 @@
- init CREATE TABLE IF NOT EXISTS tasks (
+CREATE TABLE IF NOT EXISTS tasks (
   id     SERIAL PRIMARY KEY,
   name   VARCHAR(100) NOT NULL,
   status VARCHAR(20)  NOT NULL
 );
+
 INSERT INTO tasks (name, status) VALUES
   ('Milk',         'done'),
   ('Eggs',         'done'),
